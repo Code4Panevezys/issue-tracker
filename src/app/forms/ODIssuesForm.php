dba02a143fa05ecc7c4256c05bc3cdd27f500286
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-03-08 15:56:52 +0200

    form commit

--- src/app/forms/ODIssuesForm.php
+++ src/app/forms/ODIssuesForm.php
@@ -16,16 +16,43 @@ class ODIssuesForm
      * @param bool $edit
      * @return array
      */
+    public function createForm (bool $edit = false)
     {
         $form = [
+            'storageURL' => route ('admin.api.users'),
             'buttons'    => [
                 [
                     "class" => "col-centered",
+                    "label" => trans ('HCCoreUI::core.button.submit'),
                     "type"  => "submit",
                 ],
             ],
             'structure'  => [
                 [
+                    "type"            => "singleLine",
+                    "fieldID"         => "reporter_email",
+                    "label"           => trans ("IssueTracker::users.reporter_email"),
+                    "required"        => 0,
+                    "requiredVisible" => 0,
+                ], [
+                    "type"            => "singleLine",
+                    "fieldID"         => "comment",
+                    "label"           => trans ("IssueTracker::users.comment"),
+                    "required"        => 1,
+                    "requiredVisible" => 1,
+                ], [
+                    "type"            => "singleLine",
+                    "fieldID"         => "lat",
+                    "label"           => trans ("IssueTracker::users.lat"),
+                    "required"        => 0,
+                    "requiredVisible" => 0,
+                ], [
+                    "type"            => "singleLine",
+                    "fieldID"         => "lon",
+                    "label"           => trans ("IssueTracker::users.lon"),
+                    "required"        => 0,
+                    "requiredVisible" => 0,
+                ],
             ],
         ];
 
