0839603d7cf92e369ff58ddb2099bc25cea1c908
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-03-08 14:25:36 +0200

    Initial package commit

---
+++ src/app/providers/IssueTrackerServiceProvider.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace interactivesolutions\issuetracker\app\providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class IssueTrackerServiceProvider extends ServiceProvider
+{
+    /**
+     * Register commands
+     *
+     * @var array
+     */
+    protected $commands = [];
+
+    protected $namespace = 'interactivesolutions\issuetracker\app\http\controllers';
+
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot ()
+    {
+        // register artisan commands
+        $this->commands ($this->commands);
+
+        // loading views
+        $this->loadViewsFrom (__DIR__ . '/../../resources/views', 'IssueTracker');
+
+        // loading translations
+        $this->loadTranslationsFrom (__DIR__ . '/../../resources/lang', 'IssueTracker');
+
+        // registering elements to publish
+        $this->registerPublishElements ();
+
+        // registering helpers
+        $this->registerHelpers ();
+
+        // registering routes
+        $this->registerRoutes ();
+    }
+
+    /**
+     * Register helper function
+     */
+    private function registerHelpers ()
+    {
+        $filePath = __DIR__ . '/../http/helpers.php';
+
+        if (\File::isFile ($filePath))
+            require_once $filePath;
+    }
+
+    /**
+     *  Registering all vendor items which needs to be published
+     */
+    private function registerPublishElements ()
+    {
+        $directory = __DIR__ . '/../../database/migrations/';
+
+        // Publish your migrations
+        if (file_exists ($directory))
+            $this->publishes ([
+                __DIR__ . '/../../database/migrations/' => database_path ('/migrations'),
+            ], 'migrations');
+
+        $directory = __DIR__ . '/../public';
+
+        // Publishing assets
+        if (file_exists ($directory))
+            $this->publishes ([
+                __DIR__ . '/../public' => public_path ('honeycomb'),
+            ], 'public');
+    }
+
+    /**
+     * Registering routes
+     */
+    private function registerRoutes ()
+    {
+        $filePath = __DIR__ . '/../../app/honeycomb/routes.php';
+
+        if ($filePath)
+            \Route::group (['namespace' => $this->namespace], function ($router) use ($filePath) {
+                require $filePath;
+            });
+    }
+}
+
+
